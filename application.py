2fe92f54b53733801e4e9f4ada6c51f60b3e6a79
Abid Khan <abidkhan484@gmail.com>
2020-08-22 20:34:00 +0600

    simplify the command using argparser. update requirements txt file

---
+++ application.py
@@ -0,0 +1,23 @@
+import argparse
+from subprocess import run as run_shell
+from sys import exit
+
+# create parser
+parser = argparse.ArgumentParser(description='Award winning films service')
+
+# Add the arguments
+parser.add_argument('Service',metavar='service', type=str, help='"parse" to run the crawler; "serve" to serve the API')
+
+# parser.add_argument('Serve',metavar='serve', type=str, help='To serve the API')
+
+# Execute the parse_args() method
+args = parser.parse_args()
+
+if (args.Service == 'parse'):
+    run_shell(['scrapy', 'crawl', 'awardWinningFilmCrawler'])
+elif (args.Service == 'serve'):
+    run_shell(['python', 'manage.py', 'runserver', '8080'])
+else:
+    print('The argument hasn\'t specified.\nType "parse" to run the crawler or "serve" to serve API')
+    exit()
+
